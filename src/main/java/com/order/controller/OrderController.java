d083375f6539476f7850cc308cc4eea01125d47c
Navinda Lankesh <adilanga@gmail.com>
2022-05-14 22:09:45 +0530

    Swagger API Doc Init

--- src/main/java/com/order/controller/OrderController.java
+++ src/main/java/com/order/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.order.model.OrderDetail;
 import com.order.model.OrderRequest;
 import com.order.service.OrderItemService;
 import com.order.service.OrderService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -25,17 +26,22 @@ public class OrderController {
     }
 
     @PostMapping("/place-order")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Place Order Details with Order Items")
     public void add(@RequestBody OrderRequest orderRequest) {
         orderService.add(orderRequest.getOrderDetail());
         orderItemService.updateProductQtyAndSaveOrderItems(orderRequest.getOrderItems());
     }
 
     @PostMapping("/add-order")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Create Order Details")
     public void add(@RequestBody OrderDetail orderDetail) {
         orderService.add(orderDetail);
     }
 
     @GetMapping("/get-order")
+    @ApiOperation("Get All Order Details")
     @ResponseStatus(HttpStatus.OK)
     public List<OrderDetail> getAll() {
         return orderService.getAll();
@@ -43,12 +49,14 @@ public class OrderController {
 
     @GetMapping("/get-order/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Get Order Details by ID")
     public OrderDetail getById(@PathVariable String id) {
         return orderService.getById(id);
     }
 
     @DeleteMapping("/delete-order/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Delete Order Details by ID")
     public void deleteById(@PathVariable String id) {
         orderService.deleteById(id);
     }
